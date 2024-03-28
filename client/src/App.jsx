868b8124926668719f9446f13a2d27b39d0773d7
Sandesh <sandeshyele2000@gmail.com>
2024-03-28 22:52:47 +0530

    Auth added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
-
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import {
@@ -14,6 +13,7 @@ import {
 } from "./ReduxStore/taskSlice";
 import Task from "./components/Task";
 import { MdClose } from "react-icons/md";
+import ChibiMe from "./assets/chibiME.png";
 
 function App() {
   const [taskForm, setTaskForm] = useState(false);
@@ -22,6 +22,7 @@ function App() {
   const [completedTask, setCompletedTask] = useState([]);
 
   const [completedTaskVisible, setCompletedTaskVisible] = useState(false);
+  const [validUser, setValidUser] = useState(false);
 
   const formRef = useRef(null);
 
@@ -31,7 +32,7 @@ function App() {
 
   const handleAddNewTaskForm = () => {
     setTaskForm(!taskForm);
-    if(editMode){
+    if (editMode) {
       setEditMode(false);
       formRef.current.reset();
     }
@@ -125,6 +126,14 @@ function App() {
     dispatch(getSortedTasksAsync());
   };
 
+  const handleAuth = (e) => {
+    console.log(e.target.value);
+
+    if(e.target.value==="qwerty"){
+      setValidUser(true)
+    }
+  };
+
   useEffect(() => {
     // CALLING GET-ALL-TASK API
     dispatch(getAllTasksAsync());
@@ -139,203 +148,226 @@ function App() {
   }, [tasks]);
 
   return (
-    <div className="main flex gap-3 justify-center items-center  h-[90vh]  overflow-y-auto  lg:text-[16px] text-[13px]">
-      <ToastContainer />
-      <div className="flex lg:w-[50vw] w-[80vw] h-[80vh] flex-col rounded-md border-[#232323] border-2  mt-[30px]">
-        <div className="flex bg-[#1f1f1f] rounded-tl rounded-tr w-full h-[10%] justify-between">
-          <div
-            className=" w-16 m-1 flex items-center justify-center cursor-pointer text-[#868686] hover:text-white"
-            onClick={handleAddNewTaskForm}
-          >
-            Add
-          </div>
-          <div
-            className="m-1 flex items-center justify-center cursor-pointer text-[#868686] hover:text-white"
-            onClick={() => setCompletedTaskVisible(!completedTaskVisible)}
-          >
-            Completed Tasks
-          </div>
-          <div
-            className=" w-16 m-1 flex items-center justify-center cursor-pointer  text-[#868686] hover:text-white"
-            onClick={handleSort}
-          >
-            Sort
-          </div>
-        </div>
-        <div className="flex  w-full h-full flex-col gap-2 items-center overflow-auto custom-scrollbar pt-4">
-          {status === "idle" ? (
-            tasks.length > 0 ? (
-              tasks?.map((task, index) => (
-                <Task
-                  handleEdit={handleEdit}
-                  task={task}
-                  key={index}
-                  index={index}
-                />
-              ))
-            ) : (
-              <div className="flex w-full h-full items-center justify-center">
-                <div className="flex"> No tasks</div>
+    <>
+      {validUser ? (
+        <div className="main flex gap-3 justify-center items-center  h-[90vh]  overflow-y-auto  lg:text-[16px] text-[13px]">
+          <ToastContainer />
+          <div className="flex lg:w-[50vw] w-[80vw] h-[80vh] flex-col rounded-md border-[#232323] border-2  mt-[30px]">
+            <div className="flex bg-[#1f1f1f] rounded-tl rounded-tr w-full h-[10%] justify-between">
+              <div
+                className=" w-16 m-1 flex items-center justify-center cursor-pointer text-[#868686] hover:text-white"
+                onClick={handleAddNewTaskForm}
+              >
+                Add
               </div>
-            )
-          ) : (
-            <div className="flex w-full items-center h-full justify-center">
-              <div className=" h-[50px] w-[50px] rounded-full flex items-center justify-center animate-spin border-t-[5px]  border-t-[#1f1f1f] border-[5px] border-hite-400"></div>
+              <div
+                className="m-1 flex items-center justify-center cursor-pointer text-[#868686] hover:text-white"
+                onClick={() => setCompletedTaskVisible(!completedTaskVisible)}
+              >
+                Completed Tasks
+              </div>
+              <div
+                className=" w-16 m-1 flex items-center justify-center cursor-pointer  text-[#868686] hover:text-white"
+                onClick={handleSort}
+              >
+                Sort
+              </div>
             </div>
-          )}
-        </div>
-      </div>
+            <div className="flex  w-full h-full flex-col gap-2 items-center overflow-auto custom-scrollbar pt-4">
+              {status === "idle" ? (
+                tasks.length > 0 ? (
+                  tasks?.map((task, index) => (
+                    <Task
+                      handleEdit={handleEdit}
+                      task={task}
+                      key={index}
+                      index={index}
+                    />
+                  ))
+                ) : (
+                  <div className="flex w-full h-full items-center justify-center">
+                    <div className="flex"> No tasks</div>
+                  </div>
+                )
+              ) : (
+                <div className="flex w-full items-center h-full justify-center">
+                  <div className=" h-[50px] w-[50px] rounded-full flex items-center justify-center animate-spin border-t-[5px]  border-t-[#1f1f1f] border-[5px] border-hite-400"></div>
+                </div>
+              )}
+            </div>
+          </div>
 
-      <div
-        className={`flex  opacity-100 absolute z-10 w-[98vw]  transition-all duration-300 ease-in-out left-3
+          <div
+            className={`flex  opacity-100 absolute z-10 w-[98vw]  transition-all duration-300 ease-in-out left-3
          ${
            taskForm
              ? "scale-80 top-[50px] opacity-100 "
              : "scale-0 top-[-350px]  opacity-0 "
          }`}
-      >
-        <div className="flex relative justify-center w-[100%] ">
-          <div
-            className="bg-[#1f1f1f] border-[#303030] border-[1px]  rounded-md flex flex-col gap-2 p-2 overflow-auto custom-scrollbar lg:w-[30%] h-max w-9/12"
-            id="form"
           >
-            <div className="flex w-full h-10 justify-end" id="form">
+            <div className="flex relative justify-center w-[100%] ">
               <div
-                className="flex m-2 rounded-full  w-5 h-5 items-center justify-center cursor-pointer"
-                onClick={handleAddNewTaskForm}
+                className="bg-[#1f1f1f] border-[#303030] border-[1px]  rounded-md flex flex-col gap-2 p-2 overflow-auto custom-scrollbar lg:w-[30%] h-max w-9/12"
+                id="form"
               >
-                <MdClose />
-              </div>
-            </div>
-            <form
-              ref={formRef}
-              onSubmit={(e) => handleSubmit(e)}
-              className="flex flex-col gap-4 m-5"
-            >
-              <div className="flex flex-col gap-2 w-full">
-                <label>Task</label>
-                <input
-                  type="text"
-                  className="outline-none bg-[#1f1f1f] border-black border-[1px] rounded-sm h-10 p-2"
-                  id="task"
-                />
-              </div>
-
-              <div className="flex flex-col gap-2 w-full">
-                <label>Task Details</label>
-                <textarea
-                  type="text"
-                  className="outline-none custom-scrollbar bg-[#1f1f1f] border-black border-[1px] rounded-sm h-[70px] p-2"
-                  id="taskDetails"
-                />
-              </div>
-
-              <div className="flex justify-between flex-wrap gap-4">
-                <div className="flex flex-col gap-2">
-                  <label>Due Date</label>
-                  <input
-                    type="date"
-                    name=""
-                    id="date"
-                    className="outline-none bg-[#1f1f1f] border-black border-[1px] rounded-sm h-6 p-2 w-[200px]"
-                  />
-                </div>
-
-                <div className="flex flex-col">
-                  <label>Task Priority</label>
-                  <div className="flex gap-4 mt-1">
-                    <div className="flex gap-2">
-                      <input
-                        type="radio"
-                        name="rating"
-                        id="rating1"
-                        value="0"
-                      />
-                      <label> Low </label>
-                    </div>
-                    <div className="flex gap-2">
-                      <input
-                        type="radio"
-                        name="rating"
-                        id="rating2"
-                        value="1"
-                      />
-                      <label> Medium </label>
-                    </div>
-                    <div className="flex gap-2">
-                      <input
-                        type="radio"
-                        name="rating"
-                        id="rating3"
-                        value="2"
-                      />
-                      <label> High </label>
-                    </div>
+                <div className="flex w-full h-10 justify-end" id="form">
+                  <div
+                    className="flex m-2 rounded-full  w-5 h-5 items-center justify-center cursor-pointer"
+                    onClick={handleAddNewTaskForm}
+                  >
+                    <MdClose />
                   </div>
                 </div>
-              </div>
-
-              <div className="flex items-center justify-center">
-                <button
-                  type="submit"
-                  className="border-[1px] border-[#4d4d4d] mt-[30px] p-3 rounded-md hover:bg-[#1a1a1a] scale-[90%]"
+                <form
+                  ref={formRef}
+                  onSubmit={(e) => handleSubmit(e)}
+                  className="flex flex-col gap-4 m-5"
                 >
-                  {editMode ? "Update Task" : "Add Task"}
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+                  <div className="flex flex-col gap-2 w-full">
+                    <label>Task</label>
+                    <input
+                      type="text"
+                      className="outline-none bg-[#1f1f1f] border-black border-[1px] rounded-sm h-10 p-2"
+                      id="task"
+                    />
+                  </div>
 
-      <div
-        className={`flex  opacity-100 absolute z-10 w-[98vw]  transition-all duration-300 ease-in-out left-3
+                  <div className="flex flex-col gap-2 w-full">
+                    <label>Task Details</label>
+                    <textarea
+                      type="text"
+                      className="outline-none custom-scrollbar bg-[#1f1f1f] border-black border-[1px] rounded-sm h-[70px] p-2"
+                      id="taskDetails"
+                    />
+                  </div>
+
+                  <div className="flex justify-between flex-wrap gap-4">
+                    <div className="flex flex-col gap-2">
+                      <label>Due Date</label>
+                      <input
+                        type="date"
+                        name=""
+                        id="date"
+                        className="outline-none bg-[#1f1f1f] border-black border-[1px] rounded-sm h-6 p-2 w-[200px]"
+                      />
+                    </div>
+
+                    <div className="flex flex-col">
+                      <label>Task Priority</label>
+                      <div className="flex gap-4 mt-1">
+                        <div className="flex gap-2">
+                          <input
+                            type="radio"
+                            name="rating"
+                            id="rating1"
+                            value="0"
+                          />
+                          <label> Low </label>
+                        </div>
+                        <div className="flex gap-2">
+                          <input
+                            type="radio"
+                            name="rating"
+                            id="rating2"
+                            value="1"
+                          />
+                          <label> Medium </label>
+                        </div>
+                        <div className="flex gap-2">
+                          <input
+                            type="radio"
+                            name="rating"
+                            id="rating3"
+                            value="2"
+                          />
+                          <label> High </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center justify-center">
+                    <button
+                      type="submit"
+                      className="border-[1px] border-[#4d4d4d] mt-[30px] p-3 rounded-md hover:bg-[#1a1a1a] scale-[90%]"
+                    >
+                      {editMode ? "Update Task" : "Add Task"}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={`flex  opacity-100 absolute z-10 w-[98vw]  transition-all duration-300 ease-in-out left-3
         ${
           completedTaskVisible
             ? "scale-80 top-[100px] opacity-100 "
             : "scale-0 top-[-350px]  opacity-0 "
         }`}
-      >
-        <div className="flex  relative justify-center w-[100%] ">
-          <div className="bg-[#1f1f1f] border-[#303030] border-[1px]  rounded-md flex flex-col gap-2 p-2 overflow-auto custom-scrollbar lg:w-[30%] h-[28rem] w-9/12">
-            <div className="flex w-full h-10 flex-col">
-              <div className="flex justify-between">
-                <p className="p-2">Completed Tasks</p>
-                <div
-                  className="flex m-2 rounded-full  w-5 h-5 items-center justify-center cursor-pointer"
-                  onClick={()=>setCompletedTaskVisible(false)}
-                >
-                  <MdClose />
-                </div>
-              </div>
-
-              <div className="flex  w-full h-full flex-col gap-2 items-center  pt-4">
-                {status === "idle" ? (
-                  completedTask.length > 0 ? (
-                    completedTask?.map((task, index) => (
-                      <Task
-                        handleEdit={handleEdit}
-                        task={task}
-                        key={index}
-                        index={index}
-                      />
-                    ))
-                  ) : (
-                    <div className="flex w-full h-full items-center justify-center">
-                      <div className="flex"> No completed Task</div>
+          >
+            <div className="flex  relative justify-center w-[100%] ">
+              <div className="bg-[#1f1f1f] border-[#303030] border-[1px]  rounded-md flex flex-col gap-2 p-2 overflow-auto custom-scrollbar lg:w-[30%] h-[28rem] w-9/12">
+                <div className="flex w-full h-10 flex-col">
+                  <div className="flex justify-between">
+                    <p className="p-2">Completed Tasks</p>
+                    <div
+                      className="flex m-2 rounded-full  w-5 h-5 items-center justify-center cursor-pointer"
+                      onClick={() => setCompletedTaskVisible(false)}
+                    >
+                      <MdClose />
                     </div>
-                  )
-                ) : (
-                  <div className="flex w-full items-center h-full justify-center">
-                    <div className=" h-[50px] w-[50px] rounded-full flex items-center justify-center animate-spin border-t-[5px]  border-t-[#1f1f1f] border-[5px] border-hite-400"></div>
                   </div>
-                )}
+
+                  <div className="flex  w-full h-full flex-col gap-2 items-center  pt-4">
+                    {status === "idle" ? (
+                      completedTask.length > 0 ? (
+                        completedTask?.map((task, index) => (
+                          <Task
+                            handleEdit={handleEdit}
+                            task={task}
+                            key={index}
+                            index={index}
+                          />
+                        ))
+                      ) : (
+                        <div className="flex w-full h-full items-center justify-center">
+                          <div className="flex"> No completed Task</div>
+                        </div>
+                      )
+                    ) : (
+                      <div className="flex w-full items-center h-full justify-center">
+                        <div className=" h-[50px] w-[50px] rounded-full flex items-center justify-center animate-spin border-t-[5px]  border-t-[#1f1f1f] border-[5px] border-hite-400"></div>
+                      </div>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="flex overflow-hidden   items-center justify-center">
+          <div
+            className={`flex absolute z-10 w-[98vw]  transition-all duration-300 ease-in-out left-3 scale-80 top-[150px] opacity-100 `}
+          >
+            <div className="flex  relative justify-center w-[100%] ">
+              <div className="bg-[#1f1f1f] border-[#303030] border-[1px] flex-col gap-7  rounded-md  flex items-center justify-center lg:w-[30%] h-[25rem] ">
+                <p>Welcome to Taskee</p>
+                <img src={ChibiMe} alt="" className="w-[9rem] h-[9rem] bouncer " />
+
+                <input
+                  type="password"
+                  onChange={(e) => handleAuth(e)}
+                  className="outline-none bg-[#1a1a1a] border-black border-[1px] rounded-[36px] h-10 p-2 ml-4 mr-4 w-60"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
